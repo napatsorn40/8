4db13fa4efaa591965e69437e54aa0cdb2700958
napatsorn40 <161224642+napatsorn40@users.noreply.github.com>
2024-02-25 21:24:09 +0700

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,37 @@
+
+i = 1
+while i <= 21:
+    print(i, end=", ")
+    i += 2
+
+print()
+
+
+i = 2
+while i <= 17:
+    print(i, end=", ")
+    i += 3
+
+print()
+
+
+i = 100
+while i >= -90:
+    print(i, end=", ")
+    i -= 10
+
+print()
+
+
+i = -30
+while i <= 30:
+    print(i, end=", ")
+    i += 10
+
+print()
+
+
+i = 15
+while i <= 55:
+    print(i, end=", ")
+    i += 8
